9e8e4c29dbdc2303d576e1f69e4b96bc45927019
yangfeng <yangfengzzz@hotmail.com>
2021-11-23 19:27:25 +0800

    first commit

---
+++ DigitalVox3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DigitalVox3
+//
+//  Created by 杨丰 on 2021/11/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
