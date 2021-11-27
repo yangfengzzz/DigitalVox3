94337c7c84c4feac51bb7de97db038d3fc27ca59
yangfeng <yangfengzzz@hotmail.com>
2021-11-27 21:32:01 +0800

    add render pass

---
+++ vox.render/runtime/render_pipeline/render_pass.h
@@ -0,0 +1,82 @@
+//
+//  render_pass.hpp
+//  vox.render
+//
+//  Created by 杨丰 on 2021/11/27.
+//
+
+#ifndef render_pass_hpp
+#define render_pass_hpp
+
+#include "../layer.h"
+#include "../enums/camera_clear_flags.h"
+#include "maths/color.h"
+#include <Metal/Metal.h>
+#include <string>
+
+namespace vox {
+using namespace math;
+class Material;
+using MaterialPtr = std::shared_ptr<Material>;
+class Camera;
+class RenderQueue;
+
+class RenderPass {
+public:
+    std::string name;
+    bool enabled;
+    int priority;
+    MTLRenderPassDescriptor* renderTarget;
+    MaterialPtr replaceMaterial;
+    Layer mask;
+    bool renderOverride;
+    CameraClearFlags clearFlags;
+    Color clearColor;
+    
+    /// Create a RenderPass.
+    /// - Parameters:
+    ///   - name: Pass name
+    ///   - priority: Priority, less than 0 before the default pass, greater than 0 after the default pass
+    ///   - renderTarget: The specified Render Target
+    ///   - replaceMaterial: Replaced material
+    ///   - mask: Perform bit and operations with Entity.Layer to filter the objects that this Pass needs to render
+    RenderPass(const std::string& name = "",
+               int priority = 0,
+               MTLRenderPassDescriptor* renderTarget = nullptr,
+               MaterialPtr replaceMaterial = nullptr,
+               Layer mask = Layer::Everything);
+    
+    /// Rendering callback, will be executed if renderOverride is set to true.
+    /// - Parameters:
+    ///   - camera: Camera
+    ///   - opaqueQueue: Opaque queue
+    ///   - alphaTestQueue: Alpha test queue
+    ///   - transparentQueue: Transparent queue
+    virtual void render(Camera* camera, const RenderQueue& opaqueQueue,
+                        const RenderQueue& alphaTestQueue, const RenderQueue& transparentQueue) {}
+    
+    /// Post rendering callback.
+    /// - Parameters:
+    ///   - camera: Camera
+    ///   - opaqueQueue: Opaque queue
+    ///   - alphaTestQueue: Alpha test queue
+    ///   - transparentQueue: Transparent queue
+    virtual void preRender(Camera* camera, const RenderQueue& opaqueQueue,
+                           const RenderQueue& alphaTestQueue, const RenderQueue& transparentQueue) {}
+    
+    /// Post rendering callback.
+    /// - Parameters:
+    ///   - camera: Camera
+    ///   - opaqueQueue: Opaque queue
+    ///   - alphaTestQueue: Alpha test queue
+    ///   - transparentQueue: Transparent queue
+    virtual void postRender(Camera* camera, const RenderQueue& opaqueQueue,
+                            const RenderQueue& alphaTestQueue, const RenderQueue& transparentQueue) {}
+    
+private:
+    static size_t passNum;
+};
+
+}
+
+#endif /* render_pass_hpp */
---
+++ vox.render/runtime/render_pipeline/render_pass.mm
@@ -0,0 +1,32 @@
+//
+//  render_pass.cpp
+//  vox.render
+//
+//  Created by 杨丰 on 2021/11/27.
+//
+
+#include "render_pass.h"
+
+namespace vox {
+size_t RenderPass::passNum = 0;
+
+RenderPass::RenderPass(const std::string& name,
+                       int priority,
+                       MTLRenderPassDescriptor* renderTarget,
+                       MaterialPtr replaceMaterial,
+                       Layer mask) {
+    if (name != "") {
+        this->name = name;
+    } else {
+        this->name = "RENDER_PASS" + std::to_string(RenderPass::passNum);
+        RenderPass::passNum += 1;
+    }
+    enabled = true;
+    this->priority = priority;
+    this->renderTarget = renderTarget;
+    this->replaceMaterial = replaceMaterial;
+    this->mask = mask;
+    renderOverride = false; // If renderOverride is set to true, you need to implement the render method
+}
+
+}
