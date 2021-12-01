30286347dd161ab7680eedc95905abd2be09b40c
yangfengzzz <yangfengzzz@hotmail.com>
2021-12-01 20:06:52 +0800

    add ozz loader api

--- vox.render/offline/ozz_loader.cpp
+++ vox.render/offline/ozz_loader.cpp
@@ -10,8 +10,34 @@
 namespace vox {
 namespace offline {
 namespace loader {
+bool LoadSkeleton(const char* _filename, animation::Skeleton* _skeleton) {
+    return false;
+}
 
+bool LoadAnimation(const char* _filename, animation::Animation* _animation) {
+    return false;
+}
 
+bool LoadTrack(const char* _filename, animation::FloatTrack* _track) {
+    return false;
 }
+
+bool LoadTrack(const char* _filename, animation::Float2Track* _track) {
+    return false;
 }
+
+bool LoadTrack(const char* _filename, animation::Float3Track* _track) {
+    return false;
 }
+
+bool LoadTrack(const char* _filename, animation::Float4Track* _track) {
+    return false;
+}
+
+bool LoadTrack(const char* _filename, animation::QuaternionTrack* _track) {
+    return false;
+}
+
+} // loader
+} // offline
+} // vox
--- vox.render/offline/ozz_loader.h
+++ vox.render/offline/ozz_loader.h
@@ -8,13 +8,37 @@
 #ifndef ozz_loader_hpp
 #define ozz_loader_hpp
 
-#include <stdio.h>
+#include "../runtime/animation/skeleton.h"
+#include "../runtime/animation/animation.h"
+#include "../runtime/animation/track.h"
 
 namespace vox {
 namespace offline {
 namespace loader {
+// Loads a skeleton from an ozz archive file named _filename.
+// This function will fail and return false if the file cannot be opened or if
+// it is not a valid ozz skeleton archive. A valid skeleton archive can be
+// produced with ozz tools (fbx2ozz) or using ozz skeleton serialization API.
+// _filename and _skeleton must be non-nullptr.
+bool LoadSkeleton(const char* _filename, animation::Skeleton* _skeleton);
 
+// Loads an animation from an ozz archive file named _filename.
+// This function will fail and return false if the file cannot be opened or if
+// it is not a valid ozz animation archive. A valid animation archive can be
+// produced with ozz tools (fbx2ozz) or using ozz animation serialization API.
+// _filename and _animation must be non-nullptr.
+bool LoadAnimation(const char* _filename, animation::Animation* _animation);
 
+// Loads a float track from an ozz archive file named _filename.
+// This function will fail and return false if the file cannot be opened or if
+// it is not a valid ozz float track archive. A valid float track archive can be
+// produced with ozz tools (fbx2ozz) or using ozz serialization API.
+// _filename and _track must be non-nullptr.
+bool LoadTrack(const char* _filename, animation::FloatTrack* _track);
+bool LoadTrack(const char* _filename, animation::Float2Track* _track);
+bool LoadTrack(const char* _filename, animation::Float3Track* _track);
+bool LoadTrack(const char* _filename, animation::Float4Track* _track);
+bool LoadTrack(const char* _filename, animation::QuaternionTrack* _track);
 }
 }
 }
