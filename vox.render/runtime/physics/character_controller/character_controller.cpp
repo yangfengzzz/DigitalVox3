8a07091888380e1e7f048ffbe60c879ed8c62352
yangfengzzz <yangfengzzz@hotmail.com>
2021-12-03 22:00:59 +0800

    add CharacterController

---
+++ vox.render/runtime/physics/character_controller/character_controller.cpp
@@ -0,0 +1,96 @@
+//
+//  CharacterController.cpp
+//  vox.render
+//
+//  Created by 杨丰 on 2021/12/3.
+//
+
+#include "character_controller.h"
+
+namespace vox {
+namespace physics {
+CharacterController::CharacterController(Entity* entity) :
+Component(entity) {
+}
+
+PxControllerCollisionFlags CharacterController::move(const math::Float3 &disp, float minDist, float elapsedTime) {
+    return _nativeController->move(PxVec3(disp.x, disp.y, disp.z), minDist, elapsedTime, PxControllerFilters());
+}
+
+bool CharacterController::setPosition(const math::Float3 &position) {
+    return _nativeController->setPosition(PxExtendedVec3(position.x, position.y, position.z));
+}
+
+math::Float3 CharacterController::position() const {
+    auto pose = _nativeController->getPosition();
+    return math::Float3(pose.x, pose.y, pose.z);
+}
+
+bool CharacterController::setFootPosition(const math::Float3 &position) {
+    return _nativeController->setFootPosition(PxExtendedVec3(position.x, position.y, position.z));
+}
+
+math::Float3 CharacterController::footPosition() const {
+    auto pose = _nativeController->getFootPosition();
+    return math::Float3(pose.x, pose.y, pose.z);
+}
+
+void CharacterController::setStepOffset(const float offset) {
+    _nativeController->setStepOffset(offset);
+}
+
+float CharacterController::stepOffset() const {
+    return _nativeController->getStepOffset();
+}
+
+void CharacterController::setNonWalkableMode(PxControllerNonWalkableMode::Enum flag) {
+    _nativeController->setNonWalkableMode(flag);
+}
+
+PxControllerNonWalkableMode::Enum CharacterController::nonWalkableMode() const {
+    return _nativeController->getNonWalkableMode();
+}
+
+float CharacterController::contactOffset() const {
+    return _nativeController->getContactOffset();
+}
+
+void CharacterController::setContactOffset(float offset) {
+    _nativeController->setContactOffset(offset);
+}
+
+math::Float3 CharacterController::upDirection() const {
+    auto dir = _nativeController->getUpDirection();
+    return math::Float3(dir.x, dir.y, dir.z);
+}
+
+void CharacterController::setUpDirection(const math::Float3 &up) {
+    _nativeController->setUpDirection(PxVec3(up.x, up.y, up.z));
+}
+
+float CharacterController::slopeLimit() const {
+    return _nativeController->getSlopeLimit();
+}
+
+void CharacterController::setSlopeLimit(float slopeLimit) {
+    _nativeController->setSlopeLimit(slopeLimit);
+}
+
+void CharacterController::invalidateCache() {
+    _nativeController->invalidateCache();
+}
+
+void CharacterController::state(PxControllerState &state) const {
+    _nativeController->getState(state);
+}
+
+void CharacterController::stats(PxControllerStats &stats) const {
+    _nativeController->getStats(stats);
+}
+
+void CharacterController::resize(float height) {
+    _nativeController->resize(height);
+}
+
+}
+}
---
+++ vox.render/runtime/physics/character_controller/character_controller.h
@@ -0,0 +1,65 @@
+//
+//  CharacterController.hpp
+//  vox.render
+//
+//  Created by 杨丰 on 2021/12/3.
+//
+
+#ifndef CharacterController_hpp
+#define CharacterController_hpp
+
+#include "../physics.h"
+#include "../../component.h"
+
+namespace vox {
+namespace physics {
+class CharacterController:public Component {
+public:
+    CharacterController(Entity* entity);
+    
+    PxControllerCollisionFlags move(const math::Float3 &disp, float minDist, float elapsedTime);
+
+    bool setPosition(const math::Float3 &position);
+
+    math::Float3 position() const;
+
+    bool setFootPosition(const math::Float3 &position);
+
+    math::Float3 footPosition() const;
+
+    void setStepOffset(const float offset);
+
+    float stepOffset() const;
+
+    void setNonWalkableMode(PxControllerNonWalkableMode::Enum flag);
+
+    PxControllerNonWalkableMode::Enum nonWalkableMode() const;
+
+    float contactOffset() const;
+
+    void setContactOffset(float offset);
+
+    math::Float3 upDirection() const;
+
+    void setUpDirection(const math::Float3 &up);
+
+    float slopeLimit() const;
+
+    void setSlopeLimit(float slopeLimit);
+
+    void invalidateCache();
+    
+    void state(PxControllerState &state) const;
+
+    void stats(PxControllerStats &stats) const;
+
+    void resize(float height);
+    
+private:
+    PxController* _nativeController;
+};
+
+}
+}
+
+#endif /* CharacterController_hpp */
