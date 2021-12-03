8d6bcda802eea62813179a4dcad1896978763ed0
yangfengzzz <yangfengzzz@hotmail.com>
2021-12-03 14:58:31 +0800

    add PlaneColliderShape

---
+++ vox.render/runtime/physics/shape/plane_collider_shape.cpp
@@ -0,0 +1,31 @@
+//
+//  plane_collider_shape.cpp
+//  vox.render
+//
+//  Created by 杨丰 on 2021/12/3.
+//
+
+#include "plane_collider_shape.h"
+#include "../physics_manager.h"
+
+namespace vox {
+namespace physics {
+PlaneColliderShape::PlaneColliderShape() {
+    _nativeGeometry = std::make_shared<PxPlaneGeometry>();
+    _nativeShape = PhysicsManager::_nativePhysics()->createShape(*_nativeGeometry, *_nativeMaterial);
+    _nativeShape->setQueryFilterData(PxFilterData(PhysicsManager::_idGenerator++, 0, 0, 0));
+}
+
+math::Float3 PlaneColliderShape::rotation() {
+    const auto& rot = _pose.rotation;
+    return math::ToEuler(rot);
+}
+
+void PlaneColliderShape::setRotation(const math::Float3& value) {
+    _pose.rotation = math::Quaternion::FromEuler(value.x, value.y, value.z);
+    auto rot = math::Quaternion::rotateZ(_pose.rotation, M_PI * 0.5);
+    rot = math::Normalize(rot);
+}
+
+}
+}
---
+++ vox.render/runtime/physics/shape/plane_collider_shape.h
@@ -0,0 +1,34 @@
+//
+//  plane_collider_shape.hpp
+//  vox.render
+//
+//  Created by 杨丰 on 2021/12/3.
+//
+
+#ifndef plane_collider_shape_hpp
+#define plane_collider_shape_hpp
+
+#include "collider_shape.h"
+
+namespace vox {
+namespace physics {
+/**
+ * Physical collider shape plane.
+ */
+class PlaneColliderShape :public ColliderShape {
+public:
+    PlaneColliderShape();
+    
+    /**
+     * The local rotation of this plane.
+     */
+    math::Float3 rotation();
+
+    void setRotation(const math::Float3& value);
+    
+    void setWorldScale(const math::Float3& scale) {}
+};
+
+}
+}
+#endif /* plane_collider_shape_hpp */
