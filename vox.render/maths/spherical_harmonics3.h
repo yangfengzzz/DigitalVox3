1af7a1caa0444c204b8d667aa1d7d716c7985c8b
yangfeng <yangfengzzz@hotmail.com>
2021-12-06 21:07:57 +0800

    add sh in math

---
+++ vox.render/maths/spherical_harmonics3.h
@@ -0,0 +1,58 @@
+//
+//  spherical_harmonics3.hpp
+//  vox.render
+//
+//  Created by 杨丰 on 2021/12/6.
+//
+
+#ifndef spherical_harmonics3_hpp
+#define spherical_harmonics3_hpp
+
+#include "maths/vec_float.h"
+#include "maths/color.h"
+
+namespace vox {
+namespace math {
+/**
+ * Use SH3 to represent irradiance environment maps efficiently, allowing for interactive rendering of diffuse objects under distant illumination.
+ * @remarks
+ * https://graphics.stanford.edu/papers/envmap/envmap.pdf
+ * http://www.ppsloan.org/publications/StupidSH36.pdf
+ * https://google.github.io/filament/Filament.md.html#annex/sphericalharmonics
+ */
+class SphericalHarmonics3 {
+public:
+    SphericalHarmonics3();
+    
+    SphericalHarmonics3(std::array<float, 27> coefficients);
+    
+    /**
+     * Add light to SphericalHarmonics3.
+     * @param direction - Light direction
+     * @param color - Light color
+     * @param deltaSolidAngle - The delta solid angle of the light
+     */
+    void addLight(const Float3& direction, const Color& color, float deltaSolidAngle);
+    
+    /**
+     * Evaluates the color for the specified direction.
+     * @param direction - Specified direction
+     * @return out - Out color
+     */
+    Color operator()(const Float3& direction);
+    
+    /**
+     * Scale the coefficients.
+     * @param s - The amount by which to scale the SphericalHarmonics3
+     */
+    SphericalHarmonics3 operator*(float s);
+    
+private:
+  /** The coefficients of SphericalHarmonics3. */
+    std::array<float, 27> coefficients;
+};
+
+}
+}
+
+#endif /* spherical_harmonics3_hpp */
