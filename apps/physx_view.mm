c2024925aa97f257b65ebbdcb9ebce20fd35ee88
yangfengzzz <yangfengzzz@hotmail.com>
2021-12-07 08:33:13 +0800

    try to add light

--- apps/physx_view.mm
+++ apps/physx_view.mm
@@ -13,11 +13,13 @@
 #include "../vox.render/runtime/mesh/primitive_mesh.h"
 #include "../vox.render/runtime/animator.h"
 #include "../vox.render/runtime/material/unlit_material.h"
+#include "../vox.render/runtime/material/blinn_phong_material.h"
 #include "../vox.render/runtime/controls/orbit_control.h"
 #include "../vox.render/runtime/physics/static_collider.h"
 #include "../vox.render/runtime/physics/dynamic_collider.h"
 #include "../vox.render/runtime/physics/shape/box_collider_shape.h"
 #include "../vox.render/runtime/physics/shape/sphere_collider_shape.h"
+#include "../vox.render/runtime/lighting/point_light.h"
 #include <random>
 
 using namespace vox;
@@ -27,6 +29,7 @@ int main(int, char**) {
     auto engine = Engine(canvas.get());
     auto scene = engine.sceneManager().activeScene();
     scene->background.solidColor = math::Color(0.3, 0.7, 0.6, 1.0);
+    scene->ambientLight().setDiffuseSolidColor(math::Color(1,1,1));
     
     auto rootEntity = scene->createRootEntity();
     auto cameraEntity = rootEntity->createChild("camera");
@@ -35,15 +38,21 @@ int main(int, char**) {
     cameraEntity->addComponent<vox::Camera>();
     cameraEntity->addComponent<control::OrbitControl>();
     
+    // init point light
+    auto light = rootEntity->createChild("light");
+    light->transform->setPosition(0, 3, 0);
+    auto pointLight = light->addComponent<PointLight>();
+    pointLight->intensity = 0.3;
+    
     // create box test entity
     float cubeSize = 2.0;
     auto boxEntity = rootEntity->createChild("BoxEntity");
-    auto boxMtl = std::make_shared<UnlitMaterial>(&engine);
+    auto boxMtl = std::make_shared<BlinnPhongMaterial>(&engine);
     auto boxRenderer = boxEntity->addComponent<MeshRenderer>();
     boxMtl->setBaseColor(math::Color(0.8, 0.3, 0.3, 1.0));
     boxRenderer->setMesh(PrimitiveMesh::createCuboid(&engine, cubeSize, cubeSize, cubeSize));
     boxRenderer->setMaterial(boxMtl);
-
+    
     auto boxCollider = boxEntity->addComponent<physics::StaticCollider>();
     auto boxColliderShape = std::make_shared<physics::BoxColliderShape>();
     boxColliderShape->setSize(math::Float3(cubeSize, cubeSize, cubeSize));
@@ -54,13 +63,13 @@ int main(int, char**) {
     auto sphereEntity = rootEntity->createChild("SphereEntity");
     sphereEntity->transform->setPosition(math::Float3(-5, 0, 0));
     auto sphereRenderer = sphereEntity->addComponent<MeshRenderer>();
-    auto sphereMtl = std::make_shared<UnlitMaterial>(&engine);
+    auto sphereMtl = std::make_shared<BlinnPhongMaterial>(&engine);
     std::default_random_engine e;
     std::uniform_real_distribution<float> u(0, 1);
     sphereMtl->setBaseColor(math::Color(u(e), u(e), u(e), 1));
     sphereRenderer->setMesh(PrimitiveMesh::createSphere(&engine, radius));
     sphereRenderer->setMaterial(sphereMtl);
-
+    
     auto sphereCollider = sphereEntity->addComponent<physics::DynamicCollider>();
     auto sphereColliderShape = std::make_shared<physics::SphereColliderShape>();
     sphereColliderShape->setRadius(radius);
@@ -83,7 +92,7 @@ int main(int, char**) {
                 velSign = 1;
             }
             pos.x += deltaTime * vel * float(velSign);
-
+            
             entity()->transform->setPosition(pos);
         }
     };
@@ -99,13 +108,13 @@ int main(int, char**) {
             sphereRenderer = entity->getComponent<MeshRenderer>();
             u = std::uniform_real_distribution<float>(0, 1);
         }
-
+        
         void onTriggerExit(physics::ColliderShapePtr other) override {
-            static_cast<UnlitMaterial*>(sphereRenderer->getMaterial().get())->setBaseColor(math::Color(u(e), u(e), u(e), 1));
+            static_cast<BlinnPhongMaterial*>(sphereRenderer->getMaterial().get())->setBaseColor(math::Color(u(e), u(e), u(e), 1));
         }
-
+        
         void onTriggerEnter(physics::ColliderShapePtr other) override {
-            static_cast<UnlitMaterial*>(sphereRenderer->getMaterial().get())->setBaseColor(math::Color(u(e), u(e), u(e), 1));
+            static_cast<BlinnPhongMaterial*>(sphereRenderer->getMaterial().get())->setBaseColor(math::Color(u(e), u(e), u(e), 1));
         }
     };
     
