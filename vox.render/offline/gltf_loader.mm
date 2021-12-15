8ac01c40e732627cb3fce80b01fbd302d5be6058
yangfeng <yangfengzzz@hotmail.com>
2021-12-15 21:52:38 +0800

    add animation loader

--- vox.render/offline/gltf_loader.mm
+++ vox.render/offline/gltf_loader.mm
@@ -105,7 +105,114 @@ void GLTFLoader::loadMaterials(tinygltf::Model& gltfModel) {
 }
 
 void GLTFLoader::loadAnimations(tinygltf::Model& gltfModel) {
-    
+    for (tinygltf::Animation &anim : gltfModel.animations) {
+        Animation animation{};
+        animation.name = anim.name;
+        if (anim.name.empty()) {
+            animation.name = std::to_string(animations.size());
+        }
+        
+        // Samplers
+        for (auto &samp : anim.samplers) {
+            AnimationSampler sampler{};
+            
+            if (samp.interpolation == "LINEAR") {
+                sampler.interpolation = AnimationSampler::InterpolationType::LINEAR;
+            }
+            if (samp.interpolation == "STEP") {
+                sampler.interpolation = AnimationSampler::InterpolationType::STEP;
+            }
+            if (samp.interpolation == "CUBICSPLINE") {
+                sampler.interpolation = AnimationSampler::InterpolationType::CUBICSPLINE;
+            }
+            
+            // Read sampler input time values
+            {
+                const tinygltf::Accessor &accessor = gltfModel.accessors[samp.input];
+                const tinygltf::BufferView &bufferView = gltfModel.bufferViews[accessor.bufferView];
+                const tinygltf::Buffer &buffer = gltfModel.buffers[bufferView.buffer];
+                
+                assert(accessor.componentType == TINYGLTF_COMPONENT_TYPE_FLOAT);
+                
+                float *buf = new float[accessor.count];
+                memcpy(buf, &buffer.data[accessor.byteOffset + bufferView.byteOffset], accessor.count * sizeof(float));
+                for (size_t index = 0; index < accessor.count; index++) {
+                    sampler.inputs.push_back(buf[index]);
+                }
+                
+                for (auto input : sampler.inputs) {
+                    if (input < animation.start) {
+                        animation.start = input;
+                    };
+                    if (input > animation.end) {
+                        animation.end = input;
+                    }
+                }
+            }
+            
+            // Read sampler output T/R/S values
+            {
+                const tinygltf::Accessor &accessor = gltfModel.accessors[samp.output];
+                const tinygltf::BufferView &bufferView = gltfModel.bufferViews[accessor.bufferView];
+                const tinygltf::Buffer &buffer = gltfModel.buffers[bufferView.buffer];
+                
+                assert(accessor.componentType == TINYGLTF_COMPONENT_TYPE_FLOAT);
+                
+                switch (accessor.type) {
+                    case TINYGLTF_TYPE_VEC3: {
+                        std::vector<Float3> buf(accessor.count);
+                        memcpy(buf.data(), &buffer.data[accessor.byteOffset + bufferView.byteOffset], accessor.count * sizeof(Float3));
+                        for (size_t index = 0; index < accessor.count; index++) {
+                            sampler.outputsVec4.push_back(Float4(buf[index], 0.0f));
+                        }
+                        break;
+                    }
+                    case TINYGLTF_TYPE_VEC4: {
+                        std::vector<Float4> buf(accessor.count);
+                        memcpy(buf.data(), &buffer.data[accessor.byteOffset + bufferView.byteOffset], accessor.count * sizeof(Float4));
+                        for (size_t index = 0; index < accessor.count; index++) {
+                            sampler.outputsVec4.push_back(buf[index]);
+                        }
+                        break;
+                    }
+                    default: {
+                        std::cout << "unknown type" << std::endl;
+                        break;
+                    }
+                }
+            }
+            
+            animation.samplers.push_back(sampler);
+        }
+        
+        // Channels
+        for (auto &source: anim.channels) {
+            AnimationChannel channel{};
+            
+            if (source.target_path == "rotation") {
+                channel.path = AnimationChannel::PathType::ROTATION;
+            }
+            if (source.target_path == "translation") {
+                channel.path = AnimationChannel::PathType::TRANSLATION;
+            }
+            if (source.target_path == "scale") {
+                channel.path = AnimationChannel::PathType::SCALE;
+            }
+            if (source.target_path == "weights") {
+                std::cout << "weights not yet supported, skipping channel" << std::endl;
+                continue;
+            }
+            channel.samplerIndex = source.sampler;
+            channel.node = nodeFromIndex(source.target_node);
+            if (!channel.node) {
+                continue;
+            }
+            
+            animation.channels.push_back(channel);
+        }
+        
+        animations.push_back(animation);
+    }
 }
 
 void GLTFLoader::getNodeDimensions(Entity* node, Float3& min, Float3& max) {
