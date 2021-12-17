bf715d1a19994eb2f6570dabf372b1d8ac57c143
yangfengzzz <yangfengzzz@hotmail.com>
2021-12-17 14:48:03 +0800

    add SkyBoxMaterial

---
+++ vox.render/runtime/sky/skybox_material.h
@@ -0,0 +1,50 @@
+//
+//  skybox_material.hpp
+//  vox.render
+//
+//  Created by 杨丰 on 2021/12/17.
+//
+
+#ifndef skybox_material_hpp
+#define skybox_material_hpp
+
+#include "../material/material.h"
+#include "maths/vec_float.h"
+
+namespace vox {
+/**
+ * SkyboxMaterial
+ */
+class SkyBoxMaterial :public Material {
+public:
+    /**
+     * Whether to decode from texture with RGBM format.
+     */
+    bool textureDecodeRGBM();
+
+    void setTextureDecodeRGBM(bool value);
+
+    /**
+     * RGBM decode factor, default 5.0.
+     */
+    float RGBMDecodeFactor();
+
+    void setRGBMDecodeFactor(float value);
+        
+    /**
+     * Texture cube map of the sky box material.
+     */
+    id<MTLTexture> textureCubeMap();
+
+    void setTextureCubeMap(id<MTLTexture> v);
+
+    SkyBoxMaterial(Engine* engine);
+    
+private:
+    static ShaderProperty _skyboxTextureProp;
+
+    math::Float2 _decodeParam = math::Float2(0, 5);
+};
+}
+
+#endif /* skybox_material_hpp */
---
+++ vox.render/runtime/sky/skybox_material.mm
@@ -0,0 +1,43 @@
+//
+//  skybox_material.cpp
+//  vox.render
+//
+//  Created by 杨丰 on 2021/12/17.
+//
+
+#include "skybox_material.h"
+
+namespace vox {
+ShaderProperty SkyBoxMaterial::_skyboxTextureProp = Shader::createProperty("u_cube", ShaderDataGroup::Enum::Material);
+
+bool SkyBoxMaterial::textureDecodeRGBM() {
+    return _decodeParam.x;
+}
+
+void SkyBoxMaterial::setTextureDecodeRGBM(bool value) {
+    _decodeParam.x = float(value);
+}
+
+float SkyBoxMaterial::RGBMDecodeFactor() {
+    return _decodeParam.y;
+}
+
+void SkyBoxMaterial::setRGBMDecodeFactor(float value) {
+    _decodeParam.y = value;
+}
+    
+id<MTLTexture> SkyBoxMaterial::textureCubeMap() {
+    return std::any_cast<id<MTLTexture>>(shaderData.getData(SkyBoxMaterial::_skyboxTextureProp));
+}
+
+void SkyBoxMaterial::setTextureCubeMap(id<MTLTexture> v) {
+    shaderData.setData(SkyBoxMaterial::_skyboxTextureProp, v);
+}
+
+SkyBoxMaterial::SkyBoxMaterial(Engine* engine):
+Material(engine, Shader::find("skybox")) {
+    renderState.rasterState.cullMode = MTLCullModeNone;
+    renderState.depthState.compareFunction = MTLCompareFunctionLessEqual;
+}
+
+}
