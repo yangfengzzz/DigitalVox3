e22cf63a6194f81f2cacf12cb201bd4262f6da3c
yangfeng <yangfengzzz@hotmail.com>
2021-12-24 08:15:35 +0800

    reset renderpipeline virual method

--- vox.render/runtime/render_pipeline/deferred_render_pipeline.mm
+++ vox.render/runtime/render_pipeline/deferred_render_pipeline.mm
@@ -6,6 +6,9 @@
 //
 
 #include "deferred_render_pipeline.h"
+#include "../camera.h"
+#include "../engine.h"
+#include "../renderer.h"
 
 namespace vox {
 DeferredRenderPipeline::DeferredRenderPipeline(Camera* camera):
@@ -19,7 +22,27 @@ DeferredRenderPipeline::~DeferredRenderPipeline() {
 void DeferredRenderPipeline::_drawRenderPass(RenderPass* pass, Camera* camera,
                                              std::optional<TextureCubeFace> cubeFace,
                                              int mipLevel) {
+    pass->preRender(camera, _opaqueQueue, _alphaTestQueue, _transparentQueue);
     
+    if (pass->enabled) {
+        const auto& engine = camera->engine();
+        const auto& scene = camera->scene();
+        const auto& background = scene->background;
+        auto& rhi = engine->_hardwareRenderer;
+        
+        // command encoder
+        if (pass->renderOverride) {
+            pass->render(camera, _opaqueQueue, _alphaTestQueue, _transparentQueue);
+        } else {
+            if (background.mode == BackgroundMode::Sky) {
+                _drawSky(background.sky);
+            }
+        }
+        
+        rhi.endRenderPass();// renderEncoder
+    }
+    
+    pass->postRender(camera, _opaqueQueue, _alphaTestQueue, _transparentQueue);
 }
 
 }
